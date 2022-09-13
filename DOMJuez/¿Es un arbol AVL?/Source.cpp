73a7dd1465d7b5e40c54eefddef942f4092a1f62
Carlos Gómez López <92546998+CGLMaster@users.noreply.github.com>
2022-09-13 16:23:41 +0200

    Add files via upload

--- DOMJuez/¿Es un arbol AVL?/Source.cpp
+++ DOMJuez/¿Es un arbol AVL?/Source.cpp
@@ -13,9 +13,13 @@ using namespace std;
 
 /*@ <answer>
 
- Escribe aquí un comentario general sobre la solución, explicando cómo
- se resuelve el problema y cuál es el coste de la solución, en función
- del tamaño del problema.
+    Para resolver el ejercicio me he basado primero en la estructura de como saber si un árbol es equilibrado, en este tipo de casos
+    primero se hace el caso base (si esta vacio) y el luego los casos recursivos, en los cuales compruebas tanto la derecha como la izquierda
+    Una vez definido la estructura, lo modifico para poder añadir la operación para saber si es AVL, es decir que en el nodo en el que
+    me encuentre sea mayor que todos los nodos que estén en su rama izquierda y menor que todos los nodos de su rama derecha.
+    Para ello busco el maximo y minimo de cada rama, y compruebo que el nodo cumpla estas características, junto a si es equilibrado.
+
+    El coste de la función en el peor de los casos es O(log(N)) donde N es el número de nodos del arbol.
 
  @ </answer> */
 
@@ -26,32 +30,35 @@ using namespace std;
  //@ <answer>
 
 
-bool esEquilibrado(BinTree<int> const& arbol, int& h) {
+bool esEquilibrado(BinTree<int> const& arbol, int& h, int& maxim, int& minim) { //O(log(N))
     if (arbol.empty()) {
         h = 0;
+        maxim = 0;
+        minim = 9999999;
         return true;
     }
     else {
-        int hder, hizq;
-        bool esIzq = esEquilibrado(arbol.left(), hizq);
-        bool esDer = esEquilibrado(arbol.right(), hder);
-        h = 1 + max(hizq, hder);
-        if (!arbol.left().empty()) {
-            if (arbol.left().root() > arbol.root()) return false;
+        int hder, hizq, maxizq, maxder, minizq, minder;
+        bool esIzq = esEquilibrado(arbol.left(), hizq, maxizq, minizq);
+        bool esDer = esEquilibrado(arbol.right(), hder, maxder, minder);
+        if (abs(hizq - hder) <= 1 && esIzq && esDer && arbol.root() > maxizq && arbol.root() < minder) {
+            maxim = max(arbol.root(), maxder);
+            minim = min(arbol.root(), minizq);
+            h = 1 + max(hizq, hder);
+            return true;
         }
-        if (!arbol.right().empty()) {
-            if (arbol.right().root() < arbol.root()) return false;
-        }
-        if (abs(hizq - hder) <= 1 && esIzq && esDer) return true;
         else return false;
     }
 }
 
+bool esAVL(BinTree<int> const& arbol) {
+    int h, maxim, minim;
+    return esEquilibrado(arbol, h, maxim, minim);
+}
 
 void resuelveCaso() {
-    BinTree<int> arbol = read_tree<int>(cin);
-    int h = 0;
-    if (esEquilibrado(arbol, h)) cout << "SI\n";
+    BinTree<int> arbol = read_tree<int>(cin); // O(log(N))
+    if (esAVL(arbol)) cout << "SI\n";
     else cout << "NO\n";
 }
 
