7add736ce709ed97566bf4528df753aa6ddd92fb
Carlos Gómez López <92546998+CGLMaster@users.noreply.github.com>
2022-11-15 18:39:06 +0100

    Juego de tablero Completo

--- DOMJuez/Juego de tablero/Source.cpp
+++ DOMJuez/Juego de tablero/Source.cpp
@@ -12,10 +12,18 @@ using namespace std;
 #include "Matriz.h"  // propios o los de las estructuras de datos de clase
 
 /*@ <answer>
+    
+    Para el caso recursivo básicamente te pide que compruebes las tres posiciones de arriba suya, entonces lo que hago es compruebo cual de esas tres posiciones es más grande,
+    si la diagonal izquierda, la de justo arriba o la diagonal derecha.
 
- Escribe aquí un comentario general sobre la solución, explicando cómo
- se resuelve el problema y cuál es el coste de la solución, en función
- del tamaño del problema.
+                    { tablero(i, 0) = 0     (no tiene columnas)
+    tablero(i,j)    { tablero(0, j) = 0     (no tiene filas)
+                    { tablero(i, j) += max(tablero[i - 1][j], max(tablero[i - 1][j - 1], tablero[i - 1, j + 1]))  (caso recursivo)
+
+
+    El coste de la función en tiempo en el peor de los caso es O(N^2), donde N son las dimensiones de los lados del tablero.
+    El coste de la función en espacio tambien es O(N^2)
+
 
  @ </answer> */
 
@@ -27,18 +35,18 @@ using namespace std;
 
 void maximoValor(Matriz<int> tablero, int N) {
 
-    for (int i = 1; i <= N; i++) {
-        for (int j = 1; j < N; j++) {
+    for (int i = 1; i < N; i++) {
+        for (int j = 1; j <= N; j++) {
             tablero[i][j] += max(tablero[i - 1][j], max(tablero[i - 1][j - 1], tablero[i - 1][j + 1]));
         }
     }
 
     int suma = 0;
     int pos = 0;
-    for (int i = 0; i < N; i++) {
-        if (suma < tablero[N][i]) {
-            suma = tablero[N][i];
-            pos = i + 1;
+    for (int i = 1; i <= N; i++) {
+        if (suma < tablero[N - 1][i]) {
+            suma = tablero[N - 1][i];
+            pos = i;
         }
     }
 
@@ -51,9 +59,9 @@ bool resuelveCaso() {
     cin >> N;
     if (!cin) return false;
 
-    Matriz<int> tablero(N + 1, N + 1, 0);
+    Matriz<int> tablero(N, N + 2, 0);
     for(int i = 0; i < N; i++)
-        for(int j = 0; j < N; j++)
+        for(int j = 1; j <= N; j++)
             cin >> tablero[i][j];
 
     maximoValor(tablero, N);
